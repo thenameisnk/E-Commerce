9f7c5ffc1b1a56b629ba727900f03e7afbef2744
unknown <niranjan.kamble07@gmail.com>
2022-11-10 15:40:48 +0530

    Changes in Web Site Creation

--- Web Site Creation/E-CommerceWeb/Server.js
+++ Web Site Creation/E-CommerceWeb/Server.js
@@ -2,7 +2,7 @@ var http=require('http');
 var fs=require('fs');
 
 var onReceive=function(req, res){
-    var fileName="contact.html";
+    var fileName="index.html";
     fs.readFile(fileName,'utf8',(err, data)=>{
         console.log(data);
         res.write(data);
@@ -11,5 +11,5 @@ var onReceive=function(req, res){
 
 }
 var server=http.createServer(onReceive);
-server.listen(8000);
-console.log("Online shopping is listening on port 8000");
+server.listen(8888);
+console.log("Online shopping is listening on port 8888");
